aa8093a09821263080daf34e82df920fd8213af4
Filip Kozik <536410@mail.muni.cz>
2024-12-04 13:10:30 +0100

    feat: app component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="w-[800px] mx-auto mt-4">
+      <Tabs defaultValue="products" className="w-full">
+        <nav>
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="products">Products</TabsTrigger>
+            <TabsTrigger value="categories">Categories</TabsTrigger>
+          </TabsList>
+        </nav>
+        <TabsContent value="products">
+          {/* <ProductPage /> */}
+        </TabsContent>
+        <TabsContent value="categories">
+          {/* <CategoriesPage />  */}
+        </TabsContent>
+      </Tabs>
+    </div>
   )
 }
 
